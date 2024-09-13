ee7f0a901f7a3b03fb0350ea1e8e099e74a8ca95
viralgupta <guptaviral0@gmail.com>
2024-09-13 23:05:28 +0530

    Feat: tables added

--- visualization-frontend/src/app/components/servers/live_server.tsx
+++ visualization-frontend/src/app/components/servers/live_server.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/components/ui/table";
 
 const LiveServer = () => {
   return (
-    <div className='w-full bg-blue-400 h-1/2 mb-2'>
-      LiveServer
+    <div className="w-full h-1/2 mb-2 overflow-x-scroll">
+      <Table>
+        <TableCaption>A list of active servers.</TableCaption>
+        <TableHeader>
+        <TableRow>
+            <TableHead className="w-[150px]">UUID</TableHead>
+            <TableHead>Model</TableHead>
+            <TableHead>Version</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+            <TableHead className="text-right">Learn More</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {/* <TableRow >
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow> */}
+        </TableBody>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
-export default LiveServer
+export default LiveServer;
--- visualization-frontend/src/app/components/servers/offiline_server.tsx
+++ visualization-frontend/src/app/components/servers/offiline_server.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/components/ui/table";
 
 const OfflineServer = () => {
   return (
-    <div className='w-full bg-blue-400 h-1/2 mb-2'>
-      OfflineServer
+    <div className="w-full h-1/2 mb-2 overflow-x-scroll">
+      <Table>
+        <TableCaption>A list of offline servers.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[150px]">UUID</TableHead>
+            <TableHead>Model</TableHead>
+            <TableHead>Version</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+            <TableHead className="text-right">Learn More</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {/* <TableRow >
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow> */}
+        </TableBody>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
-export default OfflineServer
+export default OfflineServer;
